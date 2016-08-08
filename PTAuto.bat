aca36e594dd79912360b632623490e2ddaa5fd79
Ravikumar G R <tara.ravikumar@gmail.com>
2016-08-08 14:48:22 +0530

    Update PTAuto.bat

--- PTAuto.bat
+++ PTAuto.bat
@@ -1,13 +1,12 @@
 @ECHO OFF
 
-set scenario=%1
-set resultsDate=%2
-set emailTo=%3
-set firstName=%4
-set lastName=%5
 
 @ECHO ON
-"C:\Program Files (x86)\HP\LoadRunner\bin\wlrun.exe" -Run -TestPath "./Scenario1.lrs"
+del /s /q c:\itqaloadtestreport\ITQA_NFT_LR_EP_OneboxUI\*
+"C:\Program Files (x86)\HP\LoadRunner\bin\wlrun.exe" -Run -TestPath "./Scenario1.lrs" -ResultName "C:\itqaloadtestreport\ITQA_NFT_LR_EP_OneboxUI\5users_2mins"
+"C:\Program Files (x86)\HP\LoadRunner\bin\AnalysisUI.exe" -RESULTPATH "C:\itqaloadtestreport\ITQA_NFT_LR_EP_OneboxUI\5users_2mins\5users_2mins.lrr"
+cd c:\Taskkill /im /F AnalysisUI.exe
+blat -html -to rkravi@juniper.net -f rkravi@juniper.net -s "ITQA-Automated-Performance-Execution-Report-%date%" -server smtp.juniper.net -port 25 -body "DO NOT REPLY TO THIS EMAIL<br />-----------------------<br /> Please find the Performance Results ITQA_NFT_LR_EP_InternalsearchUI \\p-lr-ctr1\\itqaloadtestreport\\ITQA_NFT_LR_EP_OneboxUI\\5users_2mins\\An_Report1.html
 
 exit 0
 
